3e26c755d806bd7d8c9c544c586976acde2b7536
Deep <deepgauravraj@gmail.com>
2024-09-26 13:42:15 +0530

    fix typo

--- src/error_404.rs
+++ src/error_404.rs
@@ -9,14 +9,14 @@ use crate::{
 #[component]
 pub fn NotFound() -> impl IntoView {
     view! {
-        <SiteMeta title="Not Found" description="Looks like page you're trying to vising doesnt exist" />
+        <SiteMeta title="Not Found" description="Page Not Found" />
         <Header />
 
         <div id="og-image" class="flex-grow w-full flex flex-col px-8 md:px-20 items-center justify-center">
             <h1 class="text-5xl md:text-5xl font-bold text-center"> "Page Not Found" </h1>
             <div class="h-4 flex-shrink-0" />
             <div class="text-slate-300 text-lg text-center">
-                "Looks like page you're trying to vising does not exist"
+                "Maybe it's on vacation?"
             </div>
             <div class="h-4 flex-shrink-0" />
             <a attr:aria-label={MenuPage::Home.aria_label()} href={MenuPage::Home.path()} {..view! {< {..} class="p-2 px-4 transition-colors bg-accent hover:bg-accent-dark rounded-md text-black font-medium" />}} > "Go Home" </a>
